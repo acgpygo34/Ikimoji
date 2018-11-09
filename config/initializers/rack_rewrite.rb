c789de9d182775e8c585a2b6d4eff48df8d78974
松葉岳 <takeshi-matsuba@MacBook-Pro-PP.local>
2018-11-10 06:12:10 +0900

    301redirect

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -1,7 +1,8 @@
+
 if ENV['RACK_ENV'] == 'production'
   Ikimoji::Application.config.middleware.insert_before(Rack::Runtime, ::Rack::Rewrite) do
     r301 %r{.*}, 'https://www.ikimoji.com$&', :if => Proc.new {|rack_env|
       rack_env['SERVER_NAME'] == 'ikimoji.herokuapp.com'
     }
   end
-en
+end
