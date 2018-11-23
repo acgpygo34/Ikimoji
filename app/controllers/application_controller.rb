136ee88202eabdb27cb3c0e15f54f21e33bf7d53
松葉岳 <takeshi-matsuba@MacBook-Pro-PP.local>
2018-11-23 10:47:02 +0900

    redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
   private
   def redirect_to_custom_domain
     if request.host == "ikimoji.herokuapp.com"
-      redirect_to "www.ikimoji.com"+request.path, :status => 301
+      redirect_to "www.ikimoji.com", :status => 301
+      # before
+      # redirect_to "www.ikimoji.com"+request.path, :status => 301
     end
   end
 end
