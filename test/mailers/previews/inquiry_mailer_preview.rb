39c33707709aade3bde2e098e0852fa26377614a
acg_pygo <takeshi-matsuba@MacBook-Pro-PP.local>
2018-12-08 03:09:23 +0900

    idpass=var

--- test/mailers/previews/inquiry_mailer_preview.rb
+++
@@ -1,7 +0,0 @@
-class InquiryMailerPreview < ActionMailer::Preview
-  def inquiry
-    inquiry = Inquiry.new(name: "hogehoge", message: "問い合わせメッセージ", email: "hogehoge@example.com", subject:"お問い合わせの件について")
-
-    InquiryMailer.send_mail(inquiry)
-  end
-end
